832acbf69dbc59ada2c829115fa2c7362e80bf16
rinne <180847792+rinnein@users.noreply.github.com>
2026-07-27 00:44:23 +0800

    fix: ep number parse

--- src/types-with-mods.ts
+++ src/types-with-mods.ts
@@ -5,7 +5,7 @@ export const zFastCapConf = z.object({
   f: zMods
     .refine(
       (fc) => {
-        const tempEpIds = new Set(Object.keys(fc.t).map(Number.parseInt));
+        const tempEpIds = new Set(Object.keys(fc.t).map((n) => Number.parseInt(n, 10)));
         return fc.p.every((c) => tempEpIds.has(c[3]));
       },
       { error: "所有片段的临时剧集ID必须在临时剧集表中存在" },
